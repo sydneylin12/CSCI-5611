f8f5b4a3dcfe9bdf2fcd30dc2c3994d44b1be524
sydneylin12 <sydney.lin12@gmail.com>
2020-11-26 16:10:32 -0600

    Tried to add caustics

---
+++ FishProject/Assets/Scripts/CausticsHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class CausticsHelper : MonoBehaviour
+{
+    private Projector proj;
+    public VideoPlayer vid;
+
+    void Start()
+    {
+        proj = this.gameObject.GetComponent<Projector>();
+        proj.material.SetTexture("_MainTex", vid.texture);
+        vid.isLooping = true;
+        vid.Play();
+    }
+}
